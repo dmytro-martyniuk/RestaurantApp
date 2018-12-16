796d4954123c8443996efb10e07a7cdc0806541f
Dmytro Martyniuk <dmytro.martyniuk09@gmail.com>
2018-12-16 22:47:20 +0100

    Log in manager to app

--- RestaurantApp/src/app/app-routing.module.ts
+++ RestaurantApp/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { WaiterComponent } from './waiter/waiter.component';
+import { ManagerComponent } from './managerMain/manager/manager.component';
  
  
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'manager', component: ManagerComponent },
   { path: 'waiter', component: WaiterComponent }
 ];
  
--- RestaurantApp/src/app/managerMain/manager/manager.component.ts
+++ RestaurantApp/src/app/managerMain/manager/manager.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'manager-main',
+  selector: 'manager',
   templateUrl: './manager.component.html',
   styleUrls: ['./manager.component.css']
 })
 export class ManagerComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private cookieService: CookieService,
+    private router: Router) { }
 
   ngOnInit() {
+    if(this.cookieService.get("user")!='manager'){
+      this.router.navigate(['login']);
+    }
   }
-
 }
